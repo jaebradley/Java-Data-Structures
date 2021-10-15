d65a571aa374ea82c6915ddfba7fcefc799b85ba
Jae Bradley <jae.b.bradley@gmail.com>
2021-10-15 02:49:54 -0400

    DisjointSet

---
+++ src/main/java/data_structures/sets/DisjointSet.java
@@ -0,0 +1,98 @@
+package data_structures.sets;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DisjointSet {
+    public static class UnableToIdentifyRootForValue extends Exception {
+    }
+
+    public static DisjointSet create(final Set<Integer> values) {
+        return new DisjointSet(
+                values
+                        .stream()
+                        .collect(
+                                Collectors.toMap(
+                                        Function.identity(),
+                                        (v) -> 1
+                                )
+                        ),
+                values
+                        .stream()
+                        .collect(
+                                Collectors.toMap(
+                                        Function.identity(),
+                                        Function.identity()
+                                )
+                        )
+
+        );
+    }
+
+    private final Map<Integer, Integer> sizesByRoot;
+    private final Map<Integer, Integer> parentsByChild;
+
+    private DisjointSet(final Map<Integer, Integer> sizesByRoot, final Map<Integer, Integer> parentsByChild) {
+        this.sizesByRoot = sizesByRoot;
+        this.parentsByChild = parentsByChild;
+    }
+
+    public void add(final int value) {
+        if (!parentsByChild.containsKey(value)) {
+            parentsByChild.put(value, value);
+            sizesByRoot.put(value, 1);
+        }
+    }
+
+    public int findRoot(final int value) throws UnableToIdentifyRootForValue {
+        final Integer parent = parentsByChild.get(value);
+        if (null == parent) {
+            throw new UnableToIdentifyRootForValue();
+        }
+
+        if (parent.equals(value)) {
+            return value;
+        }
+
+        final int root = findRoot(parent);
+        parentsByChild.put(value, root);
+
+        return root;
+    }
+
+    public void union(final int value1, final int value2) throws UnableToIdentifyRootForValue {
+        final int root1 = findRoot(value1);
+        final int root2 = findRoot(value2);
+
+        if (root1 != root2) {
+            final Integer root1Size = sizesByRoot.get(root1);
+            if (null == root1Size) {
+                throw new RuntimeException(createUnableToIdentifyRootSizeForValueErrorMessage(root1, value1));
+            }
+            final Integer root2Size = sizesByRoot.get(root2);
+            if (null == root2Size) {
+                throw new RuntimeException(createUnableToIdentifyRootSizeForValueErrorMessage(root2, value2));
+            }
+            final int parentRoot;
+            final int childRoot;
+            {
+                if (root1Size < root2Size) {
+                    parentRoot = root2;
+                    childRoot = root1;
+                } else {
+                    parentRoot = root1;
+                    childRoot = root2;
+                }
+            }
+
+            parentsByChild.put(childRoot, parentRoot);
+            sizesByRoot.put(parentRoot, root1Size + root2Size);
+        }
+    }
+
+    private static String createUnableToIdentifyRootSizeForValueErrorMessage(final int root, final int value) {
+        return "Unable to identify size for root " + root + " and value " + value;
+    }
+}
---
+++ src/test/java/data_structures/sets/DisjointSetTest.java
@@ -0,0 +1,306 @@
+package data_structures.sets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class DisjointSetTest {
+    @Test
+    public void TestCreatingNonEmptySets() {
+        IntStream.rangeClosed(0, 3)
+                .boxed()
+                .forEach(
+                        v -> DisjointSet.create(
+                                IntStream.rangeClosed(0, v)
+                                        .boxed()
+                                        .collect(Collectors.toSet())
+                        )
+                );
+    }
+
+    @Test
+    public void TestUnableToFindRootForValue() {
+        Assert.assertThrows(
+                DisjointSet.UnableToIdentifyRootForValue.class,
+                () -> {
+                    final DisjointSet disjointSet = DisjointSet.create(
+                            Set.of(1)
+                    );
+
+                    disjointSet.findRoot(-1);
+                }
+        );
+    }
+
+    @Test
+    public void TestAddingValue() {
+        final DisjointSet disjointSet = DisjointSet.create(Collections.emptySet());
+        try {
+            disjointSet.findRoot(1);
+            Assert.fail();
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            // expected
+        }
+
+        IntStream
+                .rangeClosed(1, 10)
+                .boxed()
+                .forEach(
+                        v -> {
+                            disjointSet.add(1);
+                            try {
+                                Assert.assertEquals(1, disjointSet.findRoot(1));
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+    }
+
+    @Test
+    public void TestAbleToFindRootForValue() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        try {
+            Assert.assertEquals(
+                    1,
+                    disjointSet.findRoot(1)
+            );
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void TestRootValueDoesNotChangeAfterMultipleCalls() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        IntStream
+                .rangeClosed(1, 10)
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        1,
+                                        disjointSet.findRoot(1)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+
+    }
+
+    @Test
+    public void TestUnionTwoValuesThatDoNotHaveRootsInSetThrowUnableToIdentifyRoot() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        Assert.assertThrows(
+                DisjointSet.UnableToIdentifyRootForValue.class,
+                () -> disjointSet.union(-1, -2)
+        );
+    }
+
+    @Test
+    public void TestUnionForFirstValueThatDoesNotHaveARootThrowsUnableToIdentifyRoot() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        Assert.assertThrows(
+                DisjointSet.UnableToIdentifyRootForValue.class,
+                () -> disjointSet.union(-1, 1)
+        );
+    }
+
+    @Test
+    public void TestUnionForSecondValueThatDoesNotHaveARootThrowsUnableToIdentifyRoot() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        Assert.assertThrows(
+                DisjointSet.UnableToIdentifyRootForValue.class,
+                () -> disjointSet.union(1, -1)
+        );
+    }
+
+    @Test
+    public void TestUnionForSameValues() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1)
+        );
+
+        try {
+            disjointSet.union(1, 1);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void TestUnionForTwoDifferentRootValues() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1, 2)
+        );
+
+        try {
+            disjointSet.union(1, 2);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        try {
+            Assert.assertEquals(
+                    1,
+                    disjointSet.findRoot(2)
+            );
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        try {
+            Assert.assertEquals(
+                    1,
+                    disjointSet.findRoot(1)
+            );
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void TestUnionForThreeDifferentRootValues() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1, 2, 3)
+        );
+
+        try {
+            disjointSet.union(1, 2);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        IntStream
+                .rangeClosed(1, 2)
+                .boxed()
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        1,
+                                        disjointSet.findRoot(v)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+
+        try {
+            disjointSet.union(2, 3);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        IntStream
+                .rangeClosed(1, 3)
+                .boxed()
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        1,
+                                        disjointSet.findRoot(v)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+    }
+
+    @Test
+    public void TestUnionForFourDifferentRootValues() {
+        final DisjointSet disjointSet = DisjointSet.create(
+                Set.of(1, 2, 3, 4)
+        );
+
+        try {
+            disjointSet.union(1, 2);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        IntStream
+                .rangeClosed(1, 2)
+                .boxed()
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        1,
+                                        disjointSet.findRoot(v)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+
+        try {
+            disjointSet.union(4, 3);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        IntStream
+                .rangeClosed(3, 4)
+                .boxed()
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        4,
+                                        disjointSet.findRoot(v)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+
+        try {
+            disjointSet.union(4, 1);
+        } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+            Assert.fail();
+        }
+
+        IntStream
+                .rangeClosed(1, 4)
+                .boxed()
+                .forEach(
+                        v -> {
+                            try {
+                                Assert.assertEquals(
+                                        4,
+                                        disjointSet.findRoot(v)
+                                );
+                            } catch (DisjointSet.UnableToIdentifyRootForValue e) {
+                                Assert.fail();
+                            }
+                        }
+                );
+    }
+}
